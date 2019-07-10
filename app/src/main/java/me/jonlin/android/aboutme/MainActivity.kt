b9529b1c18137c7965ce425874b9d6e2d863aeb3
kupolin <joanthan.lin108@gmail.com>
2019-07-10 08:14:09 -0700

    Data binding! Fun stuff.

--- app/src/main/java/me/jonlin/android/aboutme/MainActivity.kt
+++ app/src/main/java/me/jonlin/android/aboutme/MainActivity.kt
@@ -1,8 +1,8 @@
 package me.jonlin.android.aboutme
 
+import android.app.Activity
 import android.content.Context
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -10,30 +10,50 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+import me.jonlin.android.aboutme.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var myName : MyName
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val doneButton = findViewById<Button>(R.id.done_button)
-        doneButton.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        myName = MyName("Jonathan Lin", binding.nicknameEdit, binding.doneButton)
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(v: View) {
-        val nicknameText = findViewById<TextView>(R.id.nickname_text)
-        val nicknameEdit = findViewById<EditText>(R.id.nickname_edit)
 
-        //setting visibility
-        v.visibility = View.GONE //buton
-        nicknameEdit.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
+        // setting visibility
+         v.visibility = View.GONE //button
 
-        nicknameText.text = nicknameEdit.text
+//        window.currentFocus?.visibility = View.GONE
+
+        //this.myName.nickname = binding.nicknameEdit.text
+//binding.myName?.nickname = binding.nicknameEdit.text
+        binding.apply {
+        //    nicknameText.text = binding.nicknameEdit.text
+            // invalidates all binding expressions and requests a new rebind to refresh UI.
+            //invalidateOptionsMenu() force refresh also updates without invalidateAll() works
+            invalidateAll()
+        //    nicknameEdit.visibility = View.GONE
+        //    nicknameText.visibility = View.VISIBLE
+        }
+
 
         //keyboard input
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(v.windowToken, 0)
     }
+
 }
+
---
+++ app/src/main/java/me/jonlin/android/aboutme/MyName.kt
@@ -0,0 +1,6 @@
+package me.jonlin.android.aboutme
+
+import android.widget.Button
+import android.widget.EditText
+
+data class MyName (var name : String = "", val nickname : EditText, val doneButton : Button  )
